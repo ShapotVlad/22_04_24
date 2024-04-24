bced69863861191c3174e294dd771e044d9107f9
Vladimir <vlnshapat@gmail.com>
2024-04-24 14:07:02 +0300

    add sem2/task1

--- Sem2_Task1/Program.cs
+++ Sem2_Task1/Program.cs
@@ -4,8 +4,23 @@
 //a = 891 => 81
 
 
-Console.WriteLine(Введите целое трех значное число);
+Console.WriteLine("ввод трехзначного числа");
 int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstdigit = num / 100;
+    int thiddigit = num % 10;
+
+    int result = firstdigit * 10 + thiddigit;
+
+    Console.WriteLine($"вывод результата = {result}");
+}
+else
+{
+    Console.WriteLine("Ошибка! Число не трехзначное.");
+}
+
 
 
 
