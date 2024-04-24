6d1f96c99e5e3402741820d7c445c3dcf9506329
Vladimir <vlnshapat@gmail.com>
2024-04-24 17:45:58 +0300

    added sem2_task4

---
+++ sem2_task4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Программа выводит третью цифру слева
+//если  третьей цифры нет, пишет - третьей цифры нет
+//456 => 6
+//5846 => 4
+//94 => третьей цифры нет
+
+
+
+Console.WriteLine("ввод числа");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100  ||  num <= -100)
+{
+
+while (num > 999  || num < -999)
+{
+  num = num/10;  
+}
+int result = num % 10;
+
+Console.WriteLine($"Третья цифра слева = {result}");
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
